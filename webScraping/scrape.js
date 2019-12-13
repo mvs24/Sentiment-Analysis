09aae7559a37a6a442b69f4898df6b19f4ec80ad
Marius <Marius.Vasili@fti.edu.al>
2019-12-13 20:12:33 +0100

    web scraping with cheerio

---
+++ webScraping/scrape.js
@@ -0,0 +1,16 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+request("https://www.instagram.com/p/BPYgQdMgs6E/", (err, res, html) => {
+  if (!err && res.statusCode == 200) {
+    const $ = cheerio.load(html);
+
+    const commentsContainer = $('.Mr508')
+    const comments = commentsContainer.children('li a');
+    // console.log(comments)
+    comments.each((i, el) => {
+      const item = $(el).text();
+      console.log(item.children)
+    })
+  }     
+});
